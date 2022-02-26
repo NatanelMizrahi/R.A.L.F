ea81b8540b588f835488acaf845a2b438a199858
Natanel Mizrahi <nmizrahi@microsoft.com>
2022-02-26 21:09:43 +0200

    connection thread impl

--- app/src/main/java/com/example/dave/MainActivity.java
+++ app/src/main/java/com/example/dave/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.dave;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -13,6 +18,11 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.example.dave.R;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
 
     public static Handler handler;
     public static BluetoothSocket mmSocket;
-//    public static ConnectedThread connectedThread;
+    public static ConnectedThread connectedThread;
     public static CreateConnectThread createConnectThread;
 
     private final static int CONNECTING_STATUS = 1; // used in bluetooth handler to identify message status
@@ -57,9 +67,71 @@ public class MainActivity extends AppCompatActivity {
 
 
     private class CreateConnectThread extends Thread {
-        public CreateConnectThread(BluetoothAdapter bluetoothAdapter, String deviceAddress) {
-            super();
-            throw new  UnsupportedOperationException();
+        @SuppressLint("MissingPermission")
+        public CreateConnectThread(BluetoothAdapter bluetoothAdapter, String address) {
+            BluetoothDevice bluetoothDevice = bluetoothAdapter.getRemoteDevice(address);
+            BluetoothSocket tmp = null;
+            UUID uuid = bluetoothDevice.getUuids()[0].getUuid();
+
+            try {
+                /*
+                Get a BluetoothSocket to connect with the given BluetoothDevice.
+                Due to Android device varieties,the method below may not work fo different devices.
+                You should try using other methods i.e. :
+                tmp = device.createRfcommSocketToServiceRecord(MY_UUID);
+                 */
+                tmp = bluetoothDevice.createInsecureRfcommSocketToServiceRecord(uuid);
+
+            } catch (IOException e) {
+                Log.e(TAG, "Socket's create() method failed", e);
+            }
+            mmSocket = tmp;
+        }
+
+        @SuppressLint("MissingPermission")
+        public void run() {
+            // Cancel discovery because it otherwise slows down the connection.
+            BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+            bluetoothAdapter.cancelDiscovery();
+            try {
+                // Connect to the remote device through the socket. This call blocks
+                // until it succeeds or throws an exception.
+                mmSocket.connect();
+                Log.e("Status", "Device connected");
+                handler.obtainMessage(CONNECTING_STATUS, 1, -1).sendToTarget();
+            } catch (IOException connectException) {
+                // Unable to connect; close the socket and return.
+                try {
+                    mmSocket.close();
+                    Log.e("Status", "Cannot connect to device");
+                    handler.obtainMessage(CONNECTING_STATUS, -1, -1).sendToTarget();
+                } catch (IOException closeException) {
+                    Log.e(TAG, "Could not close the client socket", closeException);
+                }
+                return;
+            }
+
+            // The connection attempt succeeded. Perform work associated with
+            // the connection in a separate thread.
+            connectedThread = new ConnectedThread(mmSocket);
+            connectedThread.run();
+        }
+
+        // Closes the client socket and causes the thread to finish.
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) {
+                Log.e(TAG, "Could not close the client socket", e);
+            }
         }
     }
+    /* =============================== Thread for Data Transfer =========================================== */
+    public static class ConnectedThread extends Thread {
+
+        public ConnectedThread(BluetoothSocket mmSocket) {
+            throw new UnsupportedOperationException();
+        }
+    }
+
 }
