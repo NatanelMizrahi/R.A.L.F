584504f6cf6c578cc5a36035851a9782fa10617f
Natanel Mizrahi <nmizrahi@microsoft.com>
2022-02-26 20:49:32 +0200

    add button views

--- app/src/main/java/com/example/dave/MainActivity.java
+++ app/src/main/java/com/example/dave/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.dave;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.example.dave.R;
 
 public class MainActivity extends AppCompatActivity {
+    private BluetoothAdapter bluetoothAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button bluethoothConnectButton = findViewById(R.id.bluethoothConnectButton);
+        final ProgressBar progressBar = findViewById(R.id.progressBar);
+        final TextView connectedText = findViewById(R.id.connectedText);
+        final ImageButton forwardButton = findViewById(R.id.forwardButton);
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
     }
 }
