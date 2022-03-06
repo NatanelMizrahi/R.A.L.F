f6d457c08315b910cbed85daab2697103deff975
Natanel Mizrahi <nmizrahi@microsoft.com>
2022-03-06 08:30:45 +0200

    added alarm clock

--- app/bot/bot.ino
+++ app/bot/bot.ino
@@ -1,3 +1,5 @@
+#include <Tone.h>
+
 typedef enum {
     MOVE,
     STOP,
@@ -26,6 +28,24 @@ typedef enum {
     BACK = 3,
 } direction_t;
 
+Tone notePlayer[2];
+
+//int notes[] = { NOTE_A3,
+//                NOTE_B3,
+//                NOTE_C4,
+//                NOTE_D4,
+//                NOTE_E4,
+//                NOTE_F4,
+//                NOTE_G4 };
+
+
+int notes[] = {
+ 15, 25, 31,33,35,37,39,41,44,46,49,52,55,58,62,65,69,73,78,82,87,93,98,104,110,117,123,131,139,147,156,165,175,185,196,208,220,233,247,262,277,294,311,330,349,370,392,415,440,466,494,523,554,587,622,659,698,740,784,831,880,932,988,1047,1109,1175,1245,1319,1397,1480,1568,1661,1760,1865,1976,2093,2217,2349,2489,2637,2794,2960,3136,3322,3520,3729,3951,4186,4435,4699,4978
+};
+
+#define N_NOTES 70
+
+int speakerPin = 13;
 
 int sensorTrigPin = 12;
 int sensorEchoPin = 11;
@@ -235,21 +255,49 @@ void checkAlarm(){
     delay(2000);
 }
 
+
 void setup() {
   init_direction_pins();
 
   pinMode(sensorTrigPin, OUTPUT);
   pinMode(sensorEchoPin, INPUT);
+  pinMode(speakerPin, OUTPUT);
+
+  notePlayer[0].begin(13);
+  notePlayer[1].begin(A1);
   Serial.begin(9600);
 }
 
+int i = 0;
+int randNote;
+int randIter;
+
+void playNightmareFuel()
+{
+  int randNote = random(N_NOTES);
+  notePlayer[0].play(notes[random(N_NOTES)]);
+  randIter = random(10) * 2;
+  while (--randIter > 0) {
+      notePlayer[1].play(notes[random(N_NOTES)]);    
+      delay(200); 
+  }
+}
+
+void raiseHell(){
+  checkAndAvoidObstacles();
+  int _direction = random(FORWARD, BACK+1);
+  stopMove();
+  startMove(_direction);
+  playNightmareFuel();
+}
 void loop() {
   if (alarmSet){
     checkAlarm();
   }
   if (mode == ANARCHY_MODE) {
-    checkAndAvoidObstacles();
-    moveRandomly();
+    raiseHell();
+//    checkAndAvoidObstacles();
+//    moveRandomly();
   }
   readCommand();
 }
